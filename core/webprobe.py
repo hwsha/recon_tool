8922af39d1723661583ec51a83bef58723b630b4
unknown <CCH@COE-PC-005.mazarscoe.local>
2025-04-24 11:26:07 +0100

    fixed bad httpx parameter for threads.

--- core/webprobe.py
+++ core/webprobe.py
@@ -4,7 +4,7 @@ import os
 def probe_web_services(to_probe, old_webservices):
     print("[+] Probing web services using HTTPX...")
     input = "\n".join(to_probe)
-    web_services = run_command("httpx -c 150 -nc -sc -title -td -fr -silent -p 80,443,1234,8080,8443,8000,8888,8080,8081,3000,5000,7000,9000,9090,9200,5601,10443", input_data=input)
+    web_services = run_command("httpx -t 150 -nc -sc -title -td -fr -silent -p 80,443,1234,8080,8443,8000,8888,8080,8081,3000,5000,7000,9000,9090,9200,5601,10443", input_data=input)
     print("\n[+] Web Services Found:")
     for service in web_services:
         print(service)
